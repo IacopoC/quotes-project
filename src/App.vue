11403de499496f951e467901d7cc7e9f6d591dc3
39340 <iacopo.cutino@gmail.com>
2025-08-02 11:41:11 +0200

    modifica css.

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,12 @@ header {
   max-height: 100vh;
 }
 
+.wrapper {
+  text-align: center;
+}
+
 nav {
   width: 100%;
-  font-size: 12px;
   text-align: center;
   margin-top: 2rem;
 }
@@ -39,31 +42,11 @@ nav a.router-link-exact-active:hover {
 nav a {
   display: inline-block;
   padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
   nav {
-    text-align: left;
-    margin-left: -1rem;
     font-size: 1rem;
-
     padding: 1rem 0;
     margin-top: 1rem;
   }
