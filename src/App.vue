f8fa81d92b22bd2a16c126eacfcead2f1b2ceba5
39340 <iacopo.cutino@gmail.com>
2025-08-07 11:25:36 +0200

    aggiunto footer.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import FooterMain from './components/FooterMain.vue';
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import { RouterLink, RouterView } from 'vue-router'
   </header>
 
   <RouterView />
+  <FooterMain />
 </template>
 
 <style scoped>
---
+++ src/components/FooterMain.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer-container">
+    <p>&copy; {{ currentYear }} Quotes from Ozymandias</p>
+  </footer>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const currentYear = ref(new Date().getFullYear());
+</script>
+
+<style scoped>
+.footer-container {
+  text-align: center;
+  margin-top: auto;
+  color: white;
+  width: 100%;
+}
+
+p {
+  font-family: "Lexend Giga", sans-serif;
+}
+
+</style>
