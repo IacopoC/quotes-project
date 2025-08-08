029195781b1075cfdd14747c1a6768310ab6ff43
39340 <iacopo.cutino@gmail.com>
2025-08-08 16:25:16 +0200

    aggiunta navigazione footer.

--- src/components/FooterMain.vue
+++ src/components/FooterMain.vue
@@ -1,11 +1,15 @@
 <template>
   <footer class="footer-container">
-    <p>&copy; {{ currentYear }} Quotes from Ozymandias</p>
+    <p>&copy; {{ currentYear }} Quotes from Ozymandias -
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/about">About</RouterLink>
+    </p>
   </footer>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import { RouterLink } from 'vue-router'
 const currentYear = ref(new Date().getFullYear());
 </script>
 
