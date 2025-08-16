8b86715dbf522d65f8c2acc01a825e04e071a29d
39340 <iacopo.cutino@gmail.com>
2025-08-16 10:19:11 +0200

    modifica opzioni mappa.

--- src/components/MapDisplay.vue
+++ src/components/MapDisplay.vue
@@ -2,7 +2,7 @@
 import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
 
 const center = { lat: 40.689247, lng: -74.044502 }
-const markerOptions = { position: center, label: 'L', title: 'LADY LIBERTY' }
+const markerOptions = { position: center }
 const pinOptions = { background: '#FBBC04' }
 
 const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
@@ -18,13 +18,6 @@ const mapId = import.meta.env.VITE_MAP_ID;
     :zoom="15"
   >
     <AdvancedMarker :options="markerOptions" :pin-options="pinOptions"/>
-    <AdvancedMarker :options="markerOptions">
-      <template #content>
-        <div style="background: white; color: black; padding: 5px; border-radius: 5px">
-          Custom Content
-        </div>
-      </template>
-    </AdvancedMarker>
   </GoogleMap>
 </template>
 
