2c42d3df824fd6939a59695d5e615473caaa8dec
39340 <iacopo.cutino@gmail.com>
2025-08-16 10:07:50 +0200

    aggiunti valori env.

--- src/components/MapDisplay.vue
+++ src/components/MapDisplay.vue
@@ -4,12 +4,15 @@ import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
 const center = { lat: 40.689247, lng: -74.044502 }
 const markerOptions = { position: center, label: 'L', title: 'LADY LIBERTY' }
 const pinOptions = { background: '#FBBC04' }
+
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+const mapId = import.meta.env.VITE_MAP_ID;
 </script>
 
 <template>
   <GoogleMap
-    api-key="api_key"
-    mapId="mappa_id"
+    :api-key="apiKey"
+    :mapId="mapId"
     style="width: 100%; height: 500px; padding: 25px;"
     :center="center"
     :zoom="15"
