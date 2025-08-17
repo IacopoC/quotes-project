d15f2cf40fa487c0de04e8dd8a02a39ac21b4a91
39340 <iacopo.cutino@gmail.com>
2025-08-17 21:11:21 +0200

    aggiunte informazioni e pin.

--- src/components/MapDisplay.vue
+++ src/components/MapDisplay.vue
@@ -1,26 +1,50 @@
 <script setup lang="ts">
-import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
+import { ref } from 'vue'
+import { GoogleMap, AdvancedMarker, InfoWindow } from 'vue3-google-map'
 
 const center = { lat: 45.8777451, lng: 10.7394732 }
 const markerOptions = { position: center }
 const pinOptions = { background: '#FBBC04', glyph: '★', glyphColor: 'white', borderColor: 'white' }
 
+const showInfo = ref(false);
+
 const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 const mapId = import.meta.env.VITE_MAP_ID;
+
+const toggleInfo = () => {
+  showInfo.value = !showInfo.value;
+}
 </script>
 
 <template>
   <GoogleMap
     :api-key="apiKey"
     :mapId="mapId"
-    style="width: 100%; height: 500px; padding: 25px;"
+    class="google-map"
     :center="center"
     :zoom="15"
   >
-    <AdvancedMarker :options="markerOptions" :pin-options="pinOptions"/>
+    <AdvancedMarker :options="markerOptions" :pin-options="pinOptions" @click="toggleInfo" />
+    <InfoWindow v-if="showInfo" :options="{ position: center }">
+      <div>
+        <h3>Ozymandias place</h3>
+        <p>Lat: {{ center.lat }}</p>
+        <p>Lng: {{ center.lng }}</p>
+      </div>
+    </InfoWindow>
   </GoogleMap>
 </template>
 
 <style scoped>
+h3, p {
+  color: #181818;
+}
 
+.google-map {
+  width: 100%;
+  height: 500px;
+  padding: 25px;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0,0,0,0.2);
+}
 </style>
