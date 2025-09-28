11017a8877782ef6d5a8e1c8647d70b894c3611a
39340 <iacopo.cutino@gmail.com>
2025-09-28 21:30:35 +0200

    bozza vue meta.

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createMetaManager } from 'vue-meta'
 
 const app = createApp(App)
 
+const metaManager = createMetaManager()
+app.use(metaManager)
 app.use(router)
 
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,10 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: 'Home - Ozymandias',
+        description: 'Welcome to Ozymandias'
+      }
     },
     {
       path: '/about',
@@ -16,6 +20,10 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+      meta: {
+        title: 'About - Ozymandias',
+        description: 'About of Ozymandias'
+      }
     },
   ],
 })
