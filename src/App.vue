366058944d0d0ca71771289e83277970c8a5e349
39340 <iacopo.cutino@gmail.com>
2025-11-07 14:01:25 +0100

    tolgo link titolo, modifica bozza classi css.

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@ import FooterMain from './components/FooterMain.vue';
 
 <template>
   <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-    <span class="fs-4">Quotes from Ozymandias</span> </a>
+    <p class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+    <span class="fs-4">Quotes from Ozymandias</span>
+    </p>
     <ul class="nav nav-pills">
     <li class="nav-item"><RouterLink to="/">Home</RouterLink></li>
       <li class="nav-item"><RouterLink to="/about">About</RouterLink></li>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -18,28 +18,11 @@ useHead({
 
 <template>
   <div class="about">
-    <div class="about-container">
+    <div class="py-4">
     <h2>Quotes from Ozymandias is a project about the famous poem of Shelley, linked to places where I remember the poem and music I listened to during this period.</h2>
   </div>
   </div>
 </template>
 
-<style>
-
- h2 {
-   color: white;
-   font-family: "Lexend Giga", sans-serif;
- }
-
- .about-container {
-   padding: 20px 0;
- }
-
-@media (min-width: 1024px) {
-  .about {
-    min-height: 80vh;
-    display: flex;
-    align-items: center;
-  }
-}
+<style scoped>
 </style>
