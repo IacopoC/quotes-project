26163ed8262131b6ee540d5848edd7e360f811a3
39340 <iacopo.cutino@gmail.com>
2025-11-18 10:16:50 +0100

    modifica tag testo.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -19,7 +19,7 @@ useHead({
 <template>
   <div class="about">
     <div class="py-4">
-    <h2>Quotes from Ozymandias is a project about the famous poem of Shelley, linked to places where I remember the poem and music I listened to during this period.</h2>
+    <p><strong>Quotes from Ozymandias</strong> is a project about the famous poem of Shelley, linked to places where I remember the poem and music I listened to during this period.</p>
   </div>
   </div>
 </template>
