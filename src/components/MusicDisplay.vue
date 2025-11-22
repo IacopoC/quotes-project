60c1fb0c274284cc388e2818d6c058f5f6892ec9
39340 <iacopo.cutino@gmail.com>
2025-11-22 21:37:59 +0100

    aggiunto store songs.

--- src/components/MusicDisplay.vue
+++ src/components/MusicDisplay.vue
@@ -1,84 +1,8 @@
 <script setup>
-import { ref } from 'vue'
+import { useSongsStore } from '@/stores/songs'
 import LikeButton from '@/components/LikeButton.vue'
 
-const defaultSongs = [
-  {
-    id: 1,
-    title: "Castle",
-    description: "The Castle of Ateez: running to the quiet night. ",
-    duration: "3:10",
-    youtubeEmbed: "https://www.youtube.com/embed/doWVVntzWqM",
-    likes: 0
-  },
-  {
-    id: 2,
-    title: "Lemon Drop",
-    description: "Stay up all night till sunrise.",
-    duration: "3:43",
-    youtubeEmbed: "https://www.youtube.com/embed/H4H99b1CjPU",
-    likes: 0
-  },
-  {
-    id: 3,
-    title: "Deja Vu",
-    description: "I know you get Deja Vu. ",
-    duration: "3:10",
-    youtubeEmbed: "https://www.youtube.com/embed/nlnMDttgTbk",
-    likes: 0
-  },
-  {
-    id: 4,
-    title: "Work",
-    description: "Work, going to make that money.",
-    duration: "3:14",
-    youtubeEmbed: "https://www.youtube.com/embed/VGnOpZhsPk4",
-    likes: 0
-  },
-  {
-    id: 5,
-    title: "Halazia",
-    description: "Be the light, Halazia. ",
-    duration: "4:24",
-    youtubeEmbed: "https://www.youtube.com/embed/SszP3hlQ55Y",
-    likes: 0
-  },
-  {
-    id: 6,
-    title: "Ice on My Teeth",
-    description: "We can play like tennis.",
-    duration: "3:22",
-    youtubeEmbed: "https://www.youtube.com/embed/5OflOlcHLb8",
-    likes: 0
-  }
-]
-
-const savedSongs = JSON.parse(localStorage.getItem('songs'))
-const savedLikes = JSON.parse(localStorage.getItem('likedVideos'))
-
-const songs = ref(Array.isArray(savedSongs) ? savedSongs : defaultSongs)
-const likedVideos = ref(Array.isArray(savedLikes) ? savedLikes : [])
-
-function toggleLike(songId) {
-  const song = songs.value.find(currentSong => currentSong.id === songId)
-  if (!song) return;
-
-  const isLiked = likedVideos.value.includes(songId)
-
-  if(isLiked) {
-    song.likes--
-    const indexToRemove = likedVideos.value.indexOf(songId)
-    if(indexToRemove > -1) {
-      likedVideos.value.splice(indexToRemove, 1)
-    }
-
-  } else {
-    song.likes++
-    likedVideos.value.push(songId);
-  }
-  localStorage.setItem('songs', JSON.stringify(songs.value))
-  localStorage.setItem('likedVideos', JSON.stringify(likedVideos.value))
-}
+const songsStore = useSongsStore()
 </script>
 
 <template>
@@ -88,7 +12,7 @@ function toggleLike(songId) {
       </div>
     </div>
     <div class="row">
-      <div v-for="song in songs" :key="song.id" class="col-md-6 song-item">
+      <div v-for="song in songsStore.songs" :key="song.id" class="col-md-6 song-item">
         <div class="py-4">
         <p><strong>{{ song.title }}</strong></p>
         <p>{{ song.description }}</p>
@@ -105,8 +29,8 @@ function toggleLike(songId) {
           </div>
         <LikeButton
           :current-likes="song.likes"
-          :is-liked="likedVideos.includes(song.id)"
-          @toggle-like="toggleLike(song.id)"
+          :is-liked="songsStore.likedVideos.includes(song.id)"
+          @toggle-like="songsStore.toggleLike(song.id)"
         />
       </div>
     </div>
---
+++ src/stores/songs.js
@@ -0,0 +1,84 @@
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+
+export const useSongsStore = defineStore('songs', () => {
+
+  const defaultSongs = [
+    {
+      id: 1,
+      title: "Castle",
+      description: "The Castle of Ateez: running to the quiet night.",
+      duration: "3:10",
+      youtubeEmbed: "https://www.youtube.com/embed/doWVVntzWqM",
+      likes: 0
+    },
+    {
+      id: 2,
+      title: "Lemon Drop",
+      description: "Light hit for the summer 2025 by Ateez.",
+      duration: "3:43",
+      youtubeEmbed: "https://www.youtube.com/embed/H4H99b1CjPU",
+      likes: 0
+    },
+    {
+      id: 3,
+      title: "Deja Vu",
+      description: "I know you get Deja Vu.",
+      duration: "3:10",
+      youtubeEmbed: "https://www.youtube.com/embed/nlnMDttgTbk",
+      likes: 0
+    },
+    {
+      id: 4,
+      title: "Work",
+      description: "Work, going to make that money.",
+      duration: "3:14",
+      youtubeEmbed: "https://www.youtube.com/embed/VGnOpZhsPk4",
+      likes: 0
+    },
+    {
+      id: 5,
+      title: "Halazia",
+      description: "Be the light, Halazia.",
+      duration: "4:24",
+      youtubeEmbed: "https://www.youtube.com/embed/SszP3hlQ55Y",
+      likes: 0
+    },
+    {
+      id: 6,
+      title: "Ice on My Teeth",
+      description: "We can play like tennis.",
+      duration: "3:22",
+      youtubeEmbed: "https://www.youtube.com/embed/5OflOlcHLb8",
+      likes: 0
+    }
+  ]
+
+  const songs = ref(JSON.parse(localStorage.getItem('songs')) || defaultSongs)
+  const likedVideos = ref(JSON.parse(localStorage.getItem('likedVideos')) || [])
+
+  const totalLikes = computed(() =>
+    songs.value.reduce((sum, song) => sum + song.likes, 0)
+  )
+
+  function toggleLike(songId) {
+    const song = songs.value.find(s => s.id === songId)
+    if (!song) return
+
+    const isLiked = likedVideos.value.includes(songId)
+
+    if (isLiked) {
+      song.likes--
+      likedVideos.value = likedVideos.value.filter(id => id !== songId)
+    } else {
+      song.likes++
+      likedVideos.value.push(songId)
+    }
+
+    localStorage.setItem('songs', JSON.stringify(songs.value))
+    localStorage.setItem('likedVideos', JSON.stringify(likedVideos.value))
+  }
+
+  return { songs, likedVideos, totalLikes, toggleLike }
+})
+
