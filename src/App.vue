8fe47535d12b21c6b08b86dba7ebf5d6071c3956
39340 <iacopo.cutino@gmail.com>
2025-11-29 14:26:29 +0100

    aggiunto componente HeaderMain.

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import { useSongsStore } from '@/stores/songs'
+import { RouterView } from 'vue-router'
+import HeaderMain from './components/HeaderMain.vue';
 import FooterMain from './components/FooterMain.vue';
-
-const songsStore = useSongsStore()
 </script>
 
 <template>
-  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-    <p class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-    <span class="fs-5">Quotes from Ozymandias</span>
-    </p>
-    <ul class="nav nav-pills">
-      <li class="nav-item">
-        <RouterLink to="/" class="nav-link" active-class="active" exact-active-class="active">
-          Home
-        </RouterLink>
-      </li>
-      <li class="nav-item">
-        <RouterLink to="/about" class="nav-link" active-class="active" exact-active-class="active">
-          About
-        </RouterLink>
-      </li>
-      <li class="nav-item">
-        <RouterLink to="/music" class="nav-link" active-class="active" exact-active-class="active">
-          Music
-        </RouterLink>
-      </li>
-      <li class="nav-item ms-3 d-flex align-items-center">
-        <span class="badge bg-light-subtle fs-6">
-          ❤️ {{ songsStore.totalLikes }}
-        </span>
-      </li>
-   </ul>
-  </header>
+<HeaderMain />
   <RouterView />
   <FooterMain />
 </template>
 
 <style scoped>
-
-nav {
-  font-family: "Lexend Giga", sans-serif;
-}
-
-.nav-link {
-  color: white;
-}
-
-.nav-pills .nav-link.active {
-  background-color: #dc3545;
-}
 </style>
---
+++ src/components/HeaderMain.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import { useSongsStore } from '@/stores/songs'
+
+const songsStore = useSongsStore()
+</script>
+
+<template>
+  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <p class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <span class="fs-5">Quotes from Ozymandias</span>
+    </p>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <RouterLink to="/" class="nav-link" active-class="active" exact-active-class="active">
+          Home
+        </RouterLink>
+      </li>
+      <li class="nav-item">
+        <RouterLink to="/about" class="nav-link" active-class="active" exact-active-class="active">
+          About
+        </RouterLink>
+      </li>
+      <li class="nav-item">
+        <RouterLink to="/music" class="nav-link" active-class="active" exact-active-class="active">
+          Music
+        </RouterLink>
+      </li>
+      <li class="nav-item ms-3 d-flex align-items-center">
+        <span class="badge bg-light-subtle fs-6">
+          ❤️ {{ songsStore.totalLikes }}
+        </span>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<style scoped>
+nav {
+  font-family: "Lexend Giga", sans-serif;
+}
+
+.nav-link {
+  color: white;
+}
+
+.nav-pills .nav-link.active {
+  background-color: #dc3545;
+}
+</style>
