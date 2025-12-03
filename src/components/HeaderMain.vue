b1aa91f40f1ff2f23abef8988b478e61c23625b0
39340 <iacopo.cutino@gmail.com>
2025-12-03 10:32:30 +0100

    aggiunto link home.

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -18,7 +18,7 @@ function handleResetLikes() {
 <template>
   <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
     <p class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <span class="fs-5">Quotes from Ozymandias</span>
+      <RouterLink to="/" class="text-white text-decoration-none"><span class="fs-5">Quotes from Ozymandias</span></RouterLink>
     </p>
     <ul class="nav nav-pills">
       <li class="nav-item">
