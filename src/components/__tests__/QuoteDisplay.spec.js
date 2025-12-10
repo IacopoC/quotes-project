976405897369d0853cd2109cc613dcd97babd843
39340 <iacopo.cutino@gmail.com>
2025-12-10 17:15:50 +0100

    modifica test quotes.

--- src/components/__tests__/QuoteDisplay.spec.js
+++ src/components/__tests__/QuoteDisplay.spec.js
@@ -1,10 +1,15 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { mount } from '@vue/test-utils';
+import { createPinia, setActivePinia } from 'pinia'
 import QuoteDisplay from '../QuoteDisplay.vue';
 
 describe('QuoteDisplay.vue', () => {
 
-  vi.useFakeTimers();
+  vi.useFakeTimers()
+
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
 
   it('dovrebbe renderizzare la prima citazione e il contatore', () => {
     const wrapper = mount(QuoteDisplay);
@@ -17,21 +22,21 @@ describe('QuoteDisplay.vue', () => {
 
   it('dovrebbe cambiare alla citazione successiva quando si clicca "Next"', async () => {
     const wrapper = mount(QuoteDisplay);
-    const nextButton = wrapper.find('button', { text: 'Next' });
+    const nextButton = wrapper.find('button:nth-of-type(3)');
 
 
     await nextButton.trigger('click');
 
-    const expectedQuote = "The lone and level sands stretch far away.";
+    const expectedQuote = "Who said: Two vast and trunkless legs of stone. Stand in the desert.";
     expect(wrapper.find('p').text()).toBe(expectedQuote);
-    expect(wrapper.find('.quote-counter').text()).toBe('8 | 8');
+    expect(wrapper.find('.quote-counter').text()).toBe('2 | 8');
   });
 
 
   it('dovrebbe cambiare alla citazione precedente quando si clicca "Back"', async () => {
     const wrapper = mount(QuoteDisplay);
-    const nextButton = wrapper.find('button', { text: 'Next' });
-    const backButton = wrapper.find('button', { text: 'Back' });
+    const nextButton = wrapper.find('button:nth-of-type(1)');
+    const backButton = wrapper.find('button:nth-of-type(3)');
 
 
     await nextButton.trigger('click');
@@ -39,15 +44,15 @@ describe('QuoteDisplay.vue', () => {
     await backButton.trigger('click');
 
 
-    const expectedQuote = "And wrinkled lip and sneer of cold command";
+    const expectedQuote = "The lone and level sands stretch far away.";
     expect(wrapper.find('p').text()).toBe(expectedQuote);
-    expect(wrapper.find('.quote-counter').text()).toBe('6 | 8');
+    expect(wrapper.find('.quote-counter').text()).toBe('8 | 8');
   });
 
 
   it('dovrebbe mettere in pausa il timer automatico quando si clicca "Pause"', async () => {
     const wrapper = mount(QuoteDisplay);
-    const pauseButton = wrapper.findAll('button')[1];
+    const pauseButton = wrapper.find('button:nth-of-type(2)');
 
 
     expect(pauseButton.text()).toBe('Pause');
