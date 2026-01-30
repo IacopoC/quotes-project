1bf3dc02d1158e9268c8a31fd85ec2323dc02ea3
39340 <iacopo.cutino@gmail.com>
2026-01-30 17:51:44 +0100

    aggiunto componente InfoDisplay.

---
+++ src/components/InfoDisplay.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="row align-items-md-stretch py-4">
+    <div class="col-md-6">
+      <div class="h-100 p-5 text-bg-dark rounded-3">
+        <h2>What is Ozymandias</h2>
+        <p>See in the map the Ozymandias places I discovered, learn more about the project in the about page.</p>
+        <RouterLink to="/about"><button class="btn btn-outline-light" type="button">Go to About</button></RouterLink>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+        <h2>Places in Ozymandias</h2>
+        <p>The places of Ozymandias is Ossana (TN) and Lake Ledro(TN), two places where the spirit of the poem of Shelley is very present.</p>
+        <button class="btn btn-outline-secondary" type="button">Go to Places</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/MapDisplay.vue
+++ src/components/MapDisplay.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, computed } from 'vue'
 import { GoogleMap, AdvancedMarker, InfoWindow } from 'vue3-google-map'
-import { RouterLink } from 'vue-router'
+import InfoDisplay from '@/components/InfoDisplay.vue'
 
 const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 const mapId = import.meta.env.VITE_MAP_ID
@@ -66,22 +66,7 @@ const toggleInfo = (id) => {
   </GoogleMap>
       </div>
     </div>
-    <div class="row align-items-md-stretch py-4">
-      <div class="col-md-6">
-        <div class="h-100 p-5 text-bg-dark rounded-3">
-          <h2>What is Ozymandias</h2>
-          <p>See in the map the Ozymandias places I discovered, learn more about the project in the about page.</p>
-          <RouterLink to="/about"><button class="btn btn-outline-light" type="button">Go to About</button></RouterLink>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="h-100 p-5 bg-body-tertiary border rounded-3">
-          <h2>Places in Ozymandias</h2>
-          <p>The places of Ozymandias is Ossana (TN) and Lake Ledro(TN), two places where the spirit of the poem of Shelley is very present.</p>
-          <button class="btn btn-outline-secondary" type="button">Go to Places</button>
-        </div>
-      </div>
-    </div>
+  <InfoDisplay />
 </template>
 
 <style scoped>
