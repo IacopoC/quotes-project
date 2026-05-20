d8f994f4710bfc88bd6a77f5f8a06d443651f3f8
39340 <iacopo.cutino@gmail.com>
2026-05-20 18:33:49 +0200

    aggiunto componente AboutDisplay.

---
+++ src/components/AboutDisplay.vue
@@ -0,0 +1,44 @@
+<script setup>
+import OzymandiasImage from '@/assets/img/piramide.jpg'
+</script>
+
+<template>
+  <div class="row py-4">
+    <div class="col-md-12">
+      <h2>About Ozymandias</h2>
+    </div>
+  </div>
+  <div class="row pt-4">
+    <div class="col-md-6">
+      <p><strong>Quotes from Ozymandias</strong> is a project about the famous poem of Shelley,
+        linked to places where I remember the poem and music I listened to during this period.</p>
+      <p>Here is the full poem of Shelley:</p>
+      <figure>
+        <blockquote class="blockquote">
+          <p>I met a traveller from an antique land,
+            Who said—“Two vast and trunkless legs of stone
+            Stand in the desert. . . . Near them, on the sand,
+            Half sunk a shattered visage lies, whose frown,
+            And wrinkled lip, and sneer of cold command,
+            Tell that its sculptor well those passions read
+            Which yet survive, stamped on these lifeless things,
+            The hand that mocked them, and the heart that fed;
+            And on the pedestal, these words appear:
+            My name is Ozymandias, King of Kings;
+            Look on my Works, ye Mighty, and despair!"
+            Nothing beside remains. Round the decay
+            Of that colossal Wreck, boundless and bare
+            The lone and level sands stretch far away.</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+          Percy Bysshe Shelley <cite title="Ozymandias">Ozymandias</cite>
+        </figcaption>
+      </figure>
+    </div>
+    <div class="col-md-6">
+      <img :src="OzymandiasImage" alt="Ozymandias" class="img-fluid">
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useRoute } from 'vue-router';
 import { useHead } from '@vueuse/head';
-import OzymandiasImage from '@/assets/img/piramide.jpg';
+import AboutDisplay from '@/components/AboutDisplay.vue';
 
 
 const route = useRoute();
@@ -19,42 +19,7 @@ useHead({
 
 <template>
   <main class="page">
-  <div class="row py-4">
-    <div class="col-md-12">
-      <h2>About Ozymandias</h2>
-    </div>
-  </div>
-      <div class="row pt-4">
-      <div class="col-md-6">
-        <p><strong>Quotes from Ozymandias</strong> is a project about the famous poem of Shelley,
-          linked to places where I remember the poem and music I listened to during this period.</p>
-        <p>Here is the full poem of Shelley:</p>
-      <figure>
-        <blockquote class="blockquote">
-          <p>I met a traveller from an antique land,
-            Who said—“Two vast and trunkless legs of stone
-            Stand in the desert. . . . Near them, on the sand,
-            Half sunk a shattered visage lies, whose frown,
-            And wrinkled lip, and sneer of cold command,
-            Tell that its sculptor well those passions read
-            Which yet survive, stamped on these lifeless things,
-            The hand that mocked them, and the heart that fed;
-            And on the pedestal, these words appear:
-            My name is Ozymandias, King of Kings;
-            Look on my Works, ye Mighty, and despair!"
-            Nothing beside remains. Round the decay
-            Of that colossal Wreck, boundless and bare
-            The lone and level sands stretch far away.</p>
-        </blockquote>
-        <figcaption class="blockquote-footer">
-          Percy Bysshe Shelley <cite title="Ozymandias">Ozymandias</cite>
-        </figcaption>
-      </figure>
-      </div>
-      <div class="col-md-6">
-        <img :src="OzymandiasImage" alt="Ozymandias" class="img-fluid">
-      </div>
-      </div>
+    <AboutDisplay />
   </main>
 </template>
 
